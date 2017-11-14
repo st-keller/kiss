f745772359657a06c558034760c91654d9b0b11e
Stefan Keller <st.keller@mac.com>
2017-11-14 20:09:41 +0100

    start anew, put away missteps

---
+++ src/kiss/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  kiss
+//
+//  Created by Stefan Keller on 14.11.17.
+//  Copyright © 2017 Stefan Keller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
